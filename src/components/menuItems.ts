c2526bbb44f383a2e4fc7679480367f59264f67f
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-04-17 18:19:01 -0500

    new

--- src/components/menuItems.ts
+++ src/components/menuItems.ts
@@ -4,7 +4,7 @@ const menuItems = [
     name: "Fried Calamary",
     description: "Squid, Buttermilk",
     price: 14.0,
-    image: "../../src/assets/images/Calamary.jpg",
+    image: "../../public/images/calamari.jpg",
   },
   {
     id: 2,
@@ -12,21 +12,21 @@ const menuItems = [
     description:
       "Crispy fried marinated chicken, mayo or spicy mayo, pickle slice, served on a bun",
     price: 12.0,
-    image: "../../src/assets/images/chicken.jpg",
+    image: "../../src/public/images/chicken.jpg",
   },
   {
     id: 4,
     name: "Breakfast Burger",
     description: "Beef, Egg, Bacon, Tomato, Mayo",
     price: 11.0,
-    image: "../../src/assets/images/breakfast.jpg",
+    image: "../../src/public/images/breakfast.jpg",
   },
   {
     id: 5,
     name: "Cheese Burger",
     description: "Topped with lettuce, tomato, onion, pickle slice, choice of cheese",
     price: 12.0,
-    image: "../../src/assets/images/cheeseburger.jpg",
+    image: "../../src/public/images/cheeseburger.jpg",
   },
   {
     id: 6,
@@ -34,7 +34,7 @@ const menuItems = [
     description:
       "Grilled Chicken, Corn, Black Beans, Avocado, Tomato, Cilantro, Lime, Tortilla Strips, Ranch",
     price: 16.0,
-    image: "../../src/assets/images/Southwest-salad.jpg",
+    image: "../../src/public/images/Southwest-salad.jpg",
   },
   {
     id: 7,
@@ -42,7 +42,7 @@ const menuItems = [
     description:
       "Heart of romaine, turkey breast, hard boiled egg, chopped bacon, , avocado, cheddar, choice of dressing",
     price: 11.0,
-    image: "../../src/assets/images/cobb-salad.jpg",
+    image: "../../src/public/images/cobb-salad.jpg",
   },
 ]
 
